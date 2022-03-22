abd2521469aaa00a3d4619bdecf747cd4f3561b6
Holger Brendebach <gitlab@brendy.de>
2022-03-23 00:01:07 +0100

    upload to test.pypi works

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+def read_file(filename):
+  fh = open(filename, "r")
+  try:
+      return fh.read()
+  finally:
+      fh.close()
+
 setup(
     name='beoneapi',
     version='0.2.0',
     description='Datahandling API of the OH EJP project BeONE. Fork of the discontinued bifrostapi by Martin Basterrechea.',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/HBrendy/bifrostapi',
     author="Holger Brendebach",
     author_email="pypi@brendy.de",
